ed7e3b88a45ee3f2cf79b221243578cefb3bd37a
rivetchip <me@spidery.fr>
2017-09-22 16:49:38 +0200

    add getid ; getall from project funcs

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -8,11 +8,47 @@ class UserModel extends AbstractModel {
     protected $table = 'users';
 
 
-    public function __construct()
+    public static function getIdByLogin( $email, $password )
     {
+        $model = ( new static );
+        
+        $dbh = $model->connection();
 
+        $results = $dbh->single(
+            'SELECT id FROM @users WHERE email = :email AND BINARY password = :password',
+            [ 'email' => $email, 'password' => $password ]
+        );
+
+        return ( $results ?: null );
     }
 
+    public static function getIdByEmail( $email )
+    {
+        $model = ( new static );
+        
+        $dbh = $model->connection();
+
+        $results = $dbh->single(
+            'SELECT id FROM @users WHERE email = :email',
+            [ 'email' => $email ]
+        );
+
+        return ( $results ?: null );
+    }
+
+    public static function getAllFromProject( $projectId )
+    {
+        $model = ( new static );
+        
+        $dbh = $model->connection();
+
+        $results = $dbh->all(
+            'SELECT user_id FROM @users_has_projects WHERE project_id = :projectId',
+            [ 'projectId' => $projectId ]
+        );
+
+        return ( $results ?: null );
+    }
 
 
 
