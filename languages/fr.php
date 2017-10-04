fe43e1a56fe703f2fc8ca144d3d64b6295266a70
rivetchip <me@spidery.fr>
2017-10-04 15:34:17 +0200

    remove now useless langs

--- languages/fr.php
+++ languages/fr.php
@@ -1,15 +1,4 @@
 <?php
 
 return [
-
-    'register.fullname.notset' => 'vous devez renseigner un full name',
-    'register.email.notavailable' => 'email n\'est plus disponible',
-    'register.email.notset' => 'Veuillez renseigner un mail',
-    'register.passwords.notsame' => 'Les mots de passe ne correspondent pas',
-    'register.passwords.notset' => 'Aucun mot de passe n\'est renseigné',
-    'login.authentication.failed' => 'Pseudo ou pass incorrect',
-    'login.authentication.notset' => 'veuillez renseigner des informations',
-
-
-
 ];
