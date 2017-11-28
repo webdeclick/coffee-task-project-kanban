ac3a14cf35c31f95dc16e2ef5b178dbe62cceee0
rivetchip <me@spidery.fr>
2017-11-28 11:34:49 +0100

    move canaction to the right controller

--- app/AbstractController.php
+++ app/AbstractController.php
@@ -5,6 +5,10 @@ namespace App;
 use Slim\Http\Interfaces\RequestInterface as Request;
 use Slim\Http\Interfaces\ResponseInterface as Response;
 
+use App\Models\UserModel;
+use App\Models\ProjectsModel;
+use App\Models\TasksModel;
+
 
 abstract class AbstractController {
 
@@ -32,7 +36,93 @@ abstract class AbstractController {
         $this->isLogged = $isLogged;
     }
 
+    /**
+     * Rights ; check if the use has the right to do that action
+     *
+     * @param string $table ('create'/'update'/etc)
+     * @param string $action ('project'/'task'/etc)
+     * @param int|null $projectId (project id)
+     * @param int|null  $tableId (id of the type)
+     * @return bool
+     */
+    protected function canAction( $table, $action, $projectId = null, $tableId = null )
+    {
+        $userId = $this->userId;
 
+        // u:user ; h:user has project ; a:project admin ; m:project manager ; s:user task asignee
+        $tablesPermissions = [
+            'project' => [
+                'read' => ['h'],
+                'create' => ['u'],//anyone
+                'update' => ['a'],
+                'delete' => ['a'],
+            ],
+            'category' => [
+                'read' => ['h','m','a'],
+                'create' => ['m','a'],
+                'update' => ['m','a'],
+                'delete' => ['a'],
+            ],
+            'task' => [
+                'read' => ['s','m','a'],
+                'create' => ['h'],
+                'update' => ['s'],
+                'delete' => ['s','m','a'],
+            ],
+        ];
+
+        $userPermissions = ['u'];
+
+        if( isset($projectId) )
+        {
+            $projectModel = ProjectsModel::find($projectId);
+
+            if( $projectModel ) // project exist
+            {
+                $userHasProject = UserModel::hasProject($projectId, $userId);
+    
+                $linked_admin = $projectModel->linked_admin;
+                $linked_manager = $projectModel->linked_manager;
+    
+                if( $userHasProject ) {
+                    $userPermissions[] = 'h';
+                }
+    
+                if( $userId == $linked_admin ) {
+                    $userPermissions[] = 'a';
+                }
+        
+                if( $userId == $linked_manager ) {
+                    $userPermissions[] = 'm';
+                }
+    
+                if( $table == 'task' && isset($tableId) ) // search in task
+                {
+                    $taskModel = TasksModel::find($tableId);
+
+                    if( $taskModel )
+                    {
+                        $assigned_to = $taskModel->assigned_to;
+                        
+                        if( $userId == $assigned_to ) {
+                            $userPermissions[] = 's';
+                        }
+                    }
+                }
+            }
+        }
+
+        if( !empty($tablesPermissions[$table][$action]) )
+        {
+            $tablePermissions = $tablesPermissions[$table][$action];
+
+            $result = !empty(array_intersect($userPermissions, $tablePermissions));
+
+            return $result;
+        }
+
+        return false;
+    }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////
 
