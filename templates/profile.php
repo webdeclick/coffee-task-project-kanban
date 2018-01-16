53ad97e94c6484611a228ea0de3342da26cbdab4
Samir Lerognon <qualzz20@gmail.com>
2018-01-16 14:48:47 +0100

    prfil

--- templates/profile.php
+++ templates/profile.php
@@ -2,9 +2,45 @@
 
 
 <?php include 'elements/errors.php'; ?>
+<?php echo var_dump($user); ?>
+
+<div class="wrapper">
+    <div class="profil_col"> 
+        <div class="profil_picture">
+        <img src="https://i.ytimg.com/vi/6FQsIfE7sZM/maxresdefault.jpg" alt="">
+        </div>
+        <div class="profil_projects">
+            LISTE DES PROJETS ICI
+            <br>
+            Query from database 1
+            <br>
+            Query from database 2
+            <br>
+            Query from database 3
+            <br>
+            Query from database 4
+            <br>
+            Query from database 5
+        </div>
+    </div>
+    <div class="profil_col"> 
+        <h2><?php echo $user["fullname"]; ?></h2>
+        <p>Description Profil</p>
+        <p> Task completed: X</p>
+        <button type="">Ajouter au projet</button>
+        <h2>Coordonnées</h2>
+        <p>Téléphone: FZEFAEFZEF</p>
+        <p>Téléphone: FZEFAEFZEF</p>
+        <p>Téléphone: FZEFAEFZEF</p>
+        <p>Téléphone: FZEFAEFZEF</p>
+        <p>Téléphone: FZEFAEFZEF</p>
+
+    </div>
+</div>
 
 
-<form action="/profile/update" method="post" enctype="multipart/form-data">>
+
+<!-- <form action="/profile/update" method="post" enctype="multipart/form-data">>
 
     <label>fullname</label>
     <input type="text" name="fullname">
@@ -29,7 +65,7 @@
     <button type="submit">valider</button>
 
 </form>
-
+ -->
 
 
 
