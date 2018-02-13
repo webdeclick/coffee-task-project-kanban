3f1eedbed39f49d44fa6e246b4997dbfc94ffe7b
rivetchip <me@spidery.fr>
2018-02-13 10:35:32 +0100

    add more index

--- templates/index.php
+++ templates/index.php
@@ -12,10 +12,43 @@
             Simplement travailler ensemble
         </h2>
 
-        <p class="homepage-text-desc">
+        <div class="homepage-text-desc">
             Créez un projet, ajoutez des membres à votre équipe, et commencer à assignez des tâches. La gestion d'équipe n'a jamais été plus simple et intuitive! 
-        </p>
+        </div>
 
+        <div class="perso-container">
+            <?php if( $isLogged ): ?>
+                <a href="/projects" class="button">Voir mes projets</a>
+            <?php else: ?>
+                <a href="/login" class="button">Connexion</a> - <a href="/register">Inscription</a>
+            <?php endif; ?>
+        </div>
+    </div>
+
+</section>
+
+<section class="homepage-why">
+
+    <div class="homepage-panel">
+        <h2 class="homepage-text-title">
+            Futé, agile et visuel
+        </h2>
+
+        <div class="homepage-text-desc">
+            Simple d'apparence, les tuiles ont tout ce dont vous avez besoin pour vous organiser et ne vous ennuieront jamais avec des fonctionnalités inutiles.
+        </div>
+
+        <div class="perso-container">
+            <?php if( $isLogged ): ?>
+                <a href="/projects" class="button">Voir mes projets</a>
+            <?php else: ?>
+                <a href="/login" class="button">Connexion</a> - <a href="/register">Inscription</a>
+            <?php endif; ?>
+        </div>
+    </div>
+
+    <div class="homepage-desk">
+        <img src="/img/homepage-jessica.jpg" alt="">
     </div>
 
 </section>
